2e786a1894a8da2a031c87ca076a5f699cd1f574
sfaber34 <sfaber34@gmail.com>
2024-04-18 14:41:36 -0600

    initial prototype

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -15,7 +15,8 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">BuidlGuidl Nodes</span>
+            <pre>{'/bin/bash -c "$(curl -fsSL https://nodes.buidlguidl.com/bgnodes.sh)"'}</pre>
           </h1>
           <div className="flex justify-center items-center space-x-2">
             <p className="my-2 font-medium">Connected Address:</p>
