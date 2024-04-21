4c9eef997864a50f7edc88df059dc1e1d7f14b07
sfaber34 <sfaber34@gmail.com>
2024-04-21 16:20:35 -0600

    updated bgnodes.sh to install geth/prysm pair on linux or mac

--- packages/nextjs/public/bgnodes.sh
+++ packages/nextjs/public/bgnodes.sh
@@ -15,7 +15,7 @@ show_help() {
     echo ""
 }
 
-# Process command-line options
+# ---------- Process command-line options ----------
 while getopts ":e:c:h" opt; do
   case ${opt} in
     e )
@@ -49,60 +49,98 @@ while getopts ":e:c:h" opt; do
       ;;
   esac
 done
+# ---------- Process command-line options ----------
 
 echo "Execution client selected: $e"
 echo -e "Consensus client selected: $c\n"
 
 os_name=$(uname -s)
-echo -e "The operating system is: $os_name\n"
 
-echo -e "Checking for dependencies"
-if command -v node >/dev/null 2>&1; then
-    echo "Node.js is installed. Version:"
-    node -v
-else
-    echo "Please install node.js (https://nodejs.org/en/download)"
-    exit 1
-fi
-
-if command -v yarn >/dev/null 2>&1; then
-    echo "Yarn is installed. Version:"
-    yarn -v
-else
-    echo "Please install yarn (https://yarnpkg.com/getting-started/install)"
-    exit 1
-fi
-echo -e "\n"
-
-if [ ! -d "$HOME/ethereum/consensus" ]; then
-    echo "Creating '~/ethereum/consensus'"
-    mkdir -p "$HOME/ethereum/consensus"
-fi
-
-# if [ "$e" == "geth" ]; then
-#     echo "The execution client is Geth."
-# elif [ "$e" == "reth" ]; then
-#     echo "The execution client is Reth."
-# fi
-
-if [ ! -d "$HOME/ethereum/execution" ]; then
-    echo "Creating '~/ethereum/execution'"
-    mkdir -p "$HOME/ethereum/execution"
-fi
-
-if [ "$c" == "prysm" ]; then
-  if [ ! -f "$HOME/ethereum/execution/prysm.sh" ]; then
-    echo "Installing Prysm."
-    cd "$HOME/ethereum/execution"
-    curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output prysm.sh && chmod +x prysm.sh
+# -------------------- For Linux/MacOS --------------------
+if [ "$os_name" = "Darwin" ] || [ "$os_name" = "Linux" ]; then
+  # ---------- Check For Dependencies ----------
+  echo -e "Checking for dependencies"
+  
+  if command -v brew >/dev/null 2>&1; then
+      echo "Homebrew is installed. Version:"
+      brew -v
   else
-    echo "Prysm is already installed."
+      echo "Please install Homebrew (https://brew.sh)"
+      exit 1
   fi
-elif [ "$c" == "lighthouse" ]; then
-    echo "Installing Lighthouse."
-    # Todo: Add Lighthouse install code
+
+  if command -v node >/dev/null 2>&1; then
+      echo "Node.js is installed. Version:"
+      node -v
+  else
+      echo "Please install node.js (https://nodejs.org/en/download)"
+      exit 1
+  fi
+
+  if command -v yarn >/dev/null 2>&1; then
+      echo "Yarn is installed. Version:"
+      yarn -v
+  else
+      echo "Please install yarn (https://yarnpkg.com/getting-started/install)"
+      exit 1
+  fi
+  echo -e "\n"
+  # ---------- Check For Dependencies ----------
+
+  # ---------- Install Consensus Client ----------
+  if [ "$e" == "geth" ]; then
+    if ! command -v geth >/dev/null 2>&1; then
+      echo "Installing Geth."
+      brew tap ethereum/ethereum
+      brew install ethereum
+      cd "$HOME/ethereum/execution"
+    else
+      echo "Geth is already installed. Version:"
+      geth -v
+    fi
+  elif [ "$e" == "reth" ]; then
+      echo "Installing Reth."
+      # Todo: Add reth install code
+  fi
+  # ---------- Install Consensus Client ----------
+
+  # ---------- Install Execution Client ----------
+  if [ ! -d "$HOME/ethereum/consensus" ]; then
+      echo "Creating '~/ethereum/consensus'"
+      mkdir -p "$HOME/ethereum/consensus"
+  fi
+
+    if [ ! -d "$HOME/ethereum/jwt" ]; then
+      echo "Creating '~/ethereum/jwt'"
+      mkdir -p "$HOME/ethereum/jwt"
+  fi
+
+  if [ "$c" == "prysm" ]; then
+    if [ ! -f "$HOME/ethereum/consensus/prysm.sh" ]; then
+      echo "Installing Prysm."
+      cd "$HOME/ethereum/consensus"
+      curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output prysm.sh && chmod +x prysm.sh
+
+      if ! command -v gpg >/dev/null 2>&1; then
+          echo "Installing gpg (required for jwt.hex creation)."
+          brew install gpg
+      fi
+
+      echo "Creating JWT secret."
+      ./prysm.sh beacon-chain generate-auth-secret
+      mv jwt.hex ../jwt/jwt.hex
+    else
+      echo "Prysm is already installed."
+    fi
+  elif [ "$c" == "lighthouse" ]; then
+      echo "Installing Lighthouse."
+      # Todo: Add Lighthouse install code
+  fi
+  # ---------- Install Execution Client ----------
 fi
+# -------------------- For Linux /MacOS --------------------
 
-
+# ./prysm.sh beacon-chain --execution-endpoint=http://localhost:8551 --mainnet --jwt-secret=/home/buidlguidl/ethereum/jwt.hex --grpc-gateway-host=0.0.0.0 --grpc-gateway-port=3500
+# geth --mainnet --http --http.api eth,net,engine,admin --http.addr 0.0.0.0 --authrpc.jwtsecret=../jwt.hex --syncmode full
 
 # node helloWorld.js
