1bb48bf9546416b550d7a72e95fda9cfb63ce0e0
Kcpele <fidekg123@gmail.com>
2024-08-05 19:33:56 +0100

    fix:change title name

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-4xl font-bold">🌐 BuidlGuidl Nodes 📡</span>
+            <span className="block text-4xl font-bold">🌐 BuidlGuidl Client 📡</span>
             <span className="block text-2xl mt-4">run an ethereum node in one command</span>
             <span className="block text-2xl mb-2 mt-8">mac/linux:</span>
             <pre className="bg-black text-white p-8 rounded">
