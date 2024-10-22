25838e509048eb9c275fbf21e6add6138fb1092f
Andrea Albiac <andrealbiac@gmail.com>
2024-10-22 16:41:14 +0200

    Added border to sections

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       {/* First row */}
       <div className="flex flex-row flex-wrap lg:flex-nowrap mt-10">
         {/* Introduction section */}
-        <div className="bg-[#7877FF] p-10 lg:w-[45vw]">
+        <section className="bg-[#7877FF] p-10 lg:w-[45vw]">
           <div className="flex flex-col">
             <p>
               A one line command to deploy and monitor an Ethereum Node, funded and maintained by BuidlGuidl members.
@@ -25,23 +25,23 @@ const Home: NextPage = () => {
                     node index.js
                   `}</code>
           </div>
-        </div>
+        </section>
 
         {/* Screenshot section */}
-        <div className="bg-[#DDDDDD] flex-1 p-10 flex justify-center">
+        <section className="bg-[#DDDDDD] flex-1 p-8 flex justify-center">
           <img src="computer-screenshot.png" alt="screenshot" className="object-contain" />
-        </div>
+        </section>
 
         {/* Satellite section */}
-        <div className="bg-[#12D855] p-6 w-[40vw] lg:flex-1 flex justify-center">
+        <section className="bg-[#12D855] p-6 w-[40vw] lg:flex-1 flex justify-center">
           <img src="satellite-test.png" alt="satellite" className="object-contain" />
-        </div>
+        </section>
       </div>
 
       {/* Second row */}
       <div className="flex flex-row flex-wrap lg:flex-nowrap mb-10">
         {/* Map section */}
-        <div className="bg-[#F6F6F6] p-10">
+        <section className="bg-[#F6F6F6] p-10">
           <div className="flex flex-row items-center gap-4">
             <h1 className="text-lg">📡 Clients running</h1>
             <img src="live-tag.svg" alt="live tag" className="w-16 animate-pulse-fast mb-1.5" />
@@ -49,10 +49,10 @@ const Home: NextPage = () => {
           <div className="flex items-center justify-center pt-10">
             <img src="map.png" alt="map" className="w-auto" />
           </div>
-        </div>
+        </section>
 
         {/* Docs section */}
-        <div className="bg-black p-10 lg:w-[30vw] w-full text-white">
+        <section className="bg-black p-10 lg:w-[30vw] w-full text-white">
           <h1 className="text-lg">Useful links | Docs</h1>
           <ul className="list-disc list-outside flex flex-col m-auto lg:mx-0 pl-8 lg:pl-4 ">
             <li>
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
               </a>
             </li>
           </ul>
-        </div>
+        </section>
       </div>
     </>
   );
