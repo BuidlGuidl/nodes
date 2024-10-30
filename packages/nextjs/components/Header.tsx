251aa4677d7347ce38af75aa8d4ae8adc949d41e
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-10-30 21:21:39 +0800

    re-add fixed header and blend mode to logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -8,10 +8,14 @@ import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
  */
 export const Header = () => {
   return (
-    <header className="container mx-auto">
-      <div className="p-6 lg:p-8 flex flex-wrap items-center justify-between border-l border-r border-black">
+    <header className="container mx-auto pb-24 md:pb-32 lg:pb-36 border-l border-r border-black">
+      <div className="fixed container z-10 mix-blend-difference p-6 lg:p-8">
         <Image className="w-40 md:w-auto" src="client-logo.svg" alt="logo" width={260} height={78} />
-        <RainbowKitCustomConnectButton />
+      </div>
+      <div className="fixed container z-10 p-6 lg:p-8 flex justify-end">
+        <div className="mt-2">
+          <RainbowKitCustomConnectButton />
+        </div>
       </div>
     </header>
   );
