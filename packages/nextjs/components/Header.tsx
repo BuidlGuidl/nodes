cafdeb423e3b000664ccf300a56213d89211dd66
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-10-30 22:07:30 +0800

    invert logo image

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
   return (
     <header className="container mx-auto pb-24 md:pb-32 lg:pb-36 border-l border-r border-black">
       <div className="fixed container z-10 mix-blend-difference p-6 lg:p-8">
-        <Image className="w-40 md:w-auto" src="client-logo.svg" alt="logo" width={260} height={78} />
+        <Image className="w-40 md:w-auto invert" src="client-logo.svg" alt="logo" width={260} height={78} />
       </div>
       <div className="fixed container z-10 p-6 lg:p-8 flex justify-end">
         <div className="mt-2">
